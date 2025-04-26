337530119c8b0955b6fad56470f108c098be2e43
Steve Korshakov <steve@korshakov.com>
2025-04-25 21:12:03 -0700

    ref: simplify sample build script

--- docker/nitro-hello/build.sh
+++ docker/nitro-hello/build.sh
@@ -3,21 +3,22 @@ set -e
 
 # Parameters
 ARCH=amd64
-IMAGE="public.ecr.aws/g7d0a3u1/nitro/hello:v6"
-image_tar="enclave.tar"
-image_tag="enclave:latest"
-image_eif="enclave.eif"
+IMAGE_NAME="public.ecr.aws/g7d0a3u1/nitro/hello"
+IMAGE_TAG="v6"
+
+# Clean up
+rm -f "enclave.tar"
+rm -f "enclave.eif"
 
 # Build the enclave docker image reproducibly
-rm -f $image_tar
 docker run \
     -v $(pwd):/workspace \
 	--platform linux/$ARCH \
 	gcr.io/kaniko-project/executor:v1.9.2 \
 	--reproducible \
 	--no-push \
-	--tarPath $image_tar \
-	--destination $image_tag \
+	--tarPath "enclave.tar" \
+	--destination "enclave:latest" \
   --dockerfile Dockerfile \
 	--build-arg TARGETPLATFORM=linux/$ARCH \
 	--build-arg TARGETOS=linux \
@@ -25,26 +26,25 @@ docker run \
 	--custom-platform linux/$ARCH
 
 # Print the size and hash of the image
-echo "Docker Image size: $(ls -lh $image_tar | awk '{print $5}')"
-echo "Docker Image hash: $(sha256sum $image_tar)"
+echo "Docker Image size: $(ls -lh "enclave.tar" | awk '{print $5}')"
+echo "Docker Image hash: $(sha256sum "enclave.tar")"
 
 # Export enclave image to EIF file
-rm -f $image_eif
-docker load -i $image_tar
+docker load -i "enclave.tar"
 docker run --rm --privileged \
   --platform linux/amd64 \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v "$(pwd)":/workspace \
   public.ecr.aws/g7d0a3u1/nitro/cli:v1 \
-  nitro-cli build-enclave --docker-uri $image_tag --output-file /workspace/$image_eif
+  nitro-cli build-enclave --docker-uri "enclave:latest" --output-file /workspace/enclave.eif
 
 # Export measurements
 docker run --rm \
   --platform linux/amd64 \
   -v "$(pwd)":/workspace \
   public.ecr.aws/g7d0a3u1/nitro/cli:v1 \
-  sh -c "nitro-cli describe-eif --eif-path /workspace/$image_eif > /workspace/enclave.json"
-echo "Enclave size: $(ls -lh $image_eif | awk '{print $5}')"
+  sh -c "nitro-cli describe-eif --eif-path /workspace/enclave.eif > /workspace/enclave.json"
+echo "Enclave size: $(ls -lh enclave.eif | awk '{print $5}')"
 
 # Build output image
 docker build --platform linux/amd64 -t $IMAGE -f Dockerfile.host .
